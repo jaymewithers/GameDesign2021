129dd896c52e5db25be3ec23ad029ee6572c56f1
Jayme Withers <10859571@my.uvu.edu>
2021-04-08 20:46:48 -0600

    animated cars

---
+++ SaharaSquareURP/SaharaSquareURP/Assets/Scripts/MonoEventsBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+}
